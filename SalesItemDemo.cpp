6a653365e14635f3c65b4c96c6797461225b521b
欸嘿欸嘿 <1436648676@qq.com>
2021-11-18 23:27:04 +0800

    reducement due to permission change

--- SalesItemDemo.cpp
+++ SalesItemDemo.cpp
@@ -3,11 +3,11 @@
 using namespace::std;
 
 int main(){
-    Sales_Item item1,item2;
-    item1.units_sold = 10;
-    item1.revenue = 25.0;
-    item2.units_sold = 5;
-    item2.revenue = 10;
+    Sales_Item item1("978-7-121-15535-2",1,128.0),item2("978-7-121-15535-2",10,1280.0);
+    // item1.units_sold = 10;
+    // item1.revenue = 25.0;
+    // item2.units_sold = 5;
+    // item2.revenue = 10;
     item1.combine(item2);
     cout << item1.getAll() << endl;
 }
